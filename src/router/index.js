70c6969e6d2e9ce93b839a4b9b29d63b26201caf
adumbidiot <nathaniel.daniel23@outlook.com>
2023-01-29 18:16:17 -0800

    Fix router history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 // Routes
 import Home from "../Home.vue";
@@ -30,7 +30,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history:createWebHistory(),
+    history: createWebHashHistory(),
     routes
   })
   
