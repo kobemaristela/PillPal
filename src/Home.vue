3e5fc4d245416aee75e1ed4eafa45a9a1f6ce831
Ken <kescovilla@gmail.com>
2023-02-06 15:42:58 -0800

    Homepage 90% Done

--- src/Home.vue
+++ src/Home.vue
@@ -3,6 +3,7 @@
   display: flex;
   flex-direction: row-reverse;
   margin-right: 10em;
+  margin-top: 3em;
 }
 
 .register-form-container {
@@ -16,7 +17,8 @@
   border-radius: 2em;
   display: flex;
   flex-direction: column;
-  height: 60vh;
+  height: 55vh;
+  width: 40vh;
   justify-content: center;
   padding: 3em;
   margin: 3em;
@@ -28,10 +30,16 @@
   margin-bottom: 1em;
 }
 
+label {
+    font-size: 2em;
+  }
+  
 h2 {
   font-size: 1.5em;
-  margin: 0;
-  margin-bottom: 1em;
+  margin-top: -2em;
+  margin-bottom: 2em;
+  display: flex;
+  justify-content: center;
 }
 
 .register-button-row {
@@ -39,6 +47,7 @@ h2 {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  margin-top: 2em;
 }
 
 button {
@@ -46,7 +55,7 @@ button {
   border: 0;
   border-radius: 0.5em;
   color: var(--text-color);
-  font-size: 1.3em;
+  font-size: 2em;
   padding: 0.5em;
   transition: 0.1s;
 }
@@ -59,23 +68,82 @@ input {
   background-color: var(--input-background-color);
   border: 0;
   border-radius: 1em;
-  font-size: 1.3em;
+  font-size: 2em;
   outline: none;
   padding-left: 0.5em;
 }
 
 a {
-  font-size: 1.3em;
+  font-size: 2em;
 }
 
 .container {
   padding: 0.5em;
 }
+
+/* Oval 157 */
+.oval1 {
+  position: absolute; 
+  top: 4em; 
+  left: -10em; 
+  width: 40em; 
+  height: 40em; 
+  background: #8353E24D; /* secondary-500 */
+  border-radius: 50%; 
+}
+
+.oval2 {
+  position: absolute; 
+  top: -10em; /* top: -118px;*/
+  right: -8em; 
+  width: 35em; 
+  height: 35em; 
+  background: #8353E24D; /* secondary-500 */
+  border-radius: 50%; 
+  z-index: -1;
+}
+.rectangly {
+  position: absolute; 
+  top: 37em; 
+  left: 60em; 
+  width: 100em; 
+  height: 9.5em; 
+  background: #8353E24D; 
+  border-radius: .5em;
+  z-index: -1;
+}
+.headline {
+  position: absolute; 
+  top: 8em; 
+  left: 4em;
+  font-size: 2em; 
+}
+  .logo {
+      position: absolute; 
+      padding: 4em;
+      transform: scale(4);
+      widows: 2em;
+      background-color:transparent;
+  }
 </style>
 
 <template>
+  <div class="Shapes">
+    <div class="oval1"></div>
+    <div class="oval2"></div>
+    <div class="rectangly"></div>
+  </div>
+  <div class="headline">
+    <h1>Headline 1.</h1>
+    <h1>Headline 2.</h1>
+    <h1>Headline 3.</h1>
+  </div>
+  <div class="header">
+    <div class="logo">
+      <img src="./assets/logo_small.svg" alt="PillPal" />
+    </div>
+  </div>
   <div class="container">
-    <h1>Medicine Tracker</h1>
     <div class="register-container">
       <div class="register-padding"></div>
       <div class="register-form-container">
@@ -109,6 +177,7 @@ a {
       </div>
     </div>
   </div>
+
 </template>
 <script>
 import Api from "./Api.js";
