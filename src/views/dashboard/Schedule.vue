1e82d5a4b9d4d240b4ee2a32a10af19aea1b9329
Zak Buffington <buffingtonzr@gmail.com>
2023-02-07 01:11:43 +0000

    Added calendar to schedule

--- src/views/dashboard/Schedule.vue
+++ src/views/dashboard/Schedule.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Schedule</h1>
+  <div class="calendar">
+    <calendar />
+    <router-view></router-view>
+  </div>
 </template>
+
+<script setup>
+import calendar from "../../components/calendar.vue";
+</script>
+
+<style scoped>
+.calendar {
+  background-color: white;
+  display: flex;
+}
+</style>
