21485708de1eaf50fa44b1433a0ad6ff2ede9a14
Zak Buffington <buffingtonzr@gmail.com>
2023-02-07 01:09:36 +0000

    Calendar component created.

---
+++ src/components/Calendar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import '@fullcalendar/core'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list'
+import interactionPlugin from '@fullcalendar/interaction'
+import { reactive } from 'vue'
+
+const options = reactive({
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+        left: 'prev next today',
+        center: 'title',
+        right: 'dayGridMonth dayGridWeek listDay'
+    },
+    selectable: true
+})
+</script>
+
+<template>
+    <FullCalendar v-bind:options="options"/>
+</template>
