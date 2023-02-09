105b11b43d741f685dd215ff341ce8b611627dea
BuffingtonZ <59776753+BuffingtonZ@users.noreply.github.com>
2023-02-08 19:06:55 -0600

    added working calendar to main branch, though there are still bugs

--- src/views/dashboard/Schedule.vue
+++ src/views/dashboard/Schedule.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Schedule</h1>
+  <div class="Calendar">
+    <Calendar />
+  </div>
 </template>
+
+<script setup>
+import Calendar from '../../components/Calendar.vue';
+</script>
+
+<style scoped>
+.Calendar {
+  background-color: white;
+  display: flex 1 1 0;
+}
+</style>
