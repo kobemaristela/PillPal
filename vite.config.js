d748a7226de951ce791370184d18edd0cdd39fc6
adumbidiot <nathaniel.daniel23@outlook.com>
2023-02-15 18:37:41 -0800

    Add relative base url support for vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  plugins: [vue()],
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    base: process.env["VITE_BASE"] || "/",
+    plugins: [vue()],
+  });
+};
