33bb1537b21f7f158e797f36f08cb3b3b56d5251
Kobe Maristela <kobe@maristela.net>
2023-02-16 04:15:58 +0000

    Resolve "Auto-logout the user after a set period of inactivity"

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,10 +3,12 @@
     <Sidebar />
     <router-view></router-view>
   </div>
+  <AutoLogout />
 </template>
 
 <script setup>
 import Sidebar from "../components/Sidebar.vue";
+import AutoLogout from "../components/AutoLogout.vue"
 </script>
 
 <style scoped>
