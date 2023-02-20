b7aba268e9aa260231a4bea10e1e29b53aa37886
Kobe Maristela <kobe@maristela.net>
2023-02-20 00:24:06 +0000

    Resolve "Redirect Users from Dashboard to Home if Logged Out"

---
+++ src/components/AutoRedirect.vue
@@ -0,0 +1,12 @@
+<script>
+    import Api from '../Api.js';
+
+    export default {
+        mounted() {
+            if (!Api.isLoggedIn()) {
+                this.$router.push('/');
+            }
+        }
+    };
+</script>
+  
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,11 +3,13 @@
     <Sidebar />
     <router-view></router-view>
   </div>
+  <AutoRedirect />
   <AutoLogout />
 </template>
 
 <script setup>
 import Sidebar from "../components/Sidebar.vue";
+import AutoRedirect from "../components/AutoRedirect.vue";
 import AutoLogout from "../components/AutoLogout.vue"
 </script>
 
